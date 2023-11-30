c8fe852bf86c0520e325e597b9f3e7589ad130c2
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2023-11-30 13:24:51 +0530

    Now tracking scroll position when navigating away from tweet page, and restoring it on navigating back

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,22 @@
-console.log("WOOGHOOO");
+const scrollPositionsMap = {};
+
+window.navigation.addEventListener("navigate", (evt) => {
+  const source = location.href;
+  if (!source.includes("status")) return;
+  scrollPositionsMap[source] = window.scrollY;
+
+  const destination = evt.destination.url;
+  if (destination in scrollPositionsMap) {
+    delayMs(1000).then(() =>
+      scrollTo({
+        left: 0,
+        top: scrollPositionsMap[destination],
+        // behavior: "smooth",
+      })
+    );
+  }
+});
+
+function delayMs(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
