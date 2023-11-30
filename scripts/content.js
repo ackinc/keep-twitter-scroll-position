bd783b3ecaaab050d7e3ab68a2482c47cf3d619f
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2023-11-30 19:31:12 +0530

    Now highlighting just-seen tweet on navigating back

--- scripts/content.js
+++ scripts/content.js
@@ -1,11 +1,19 @@
-const scrollPositionsMap = {};
+// PROBLEMS:
+// 1. scroll position should only be restored on backwards navigation,
+//      not forwards navigation
 
-window.navigation.addEventListener("navigate", async (evt) => {
+const ENABLE_DEBUG_LOGGING = 1;
+
+const scrollPositionsMap = {};
+window.navigation.addEventListener("navigate", handleNavigation);
+if (ENABLE_DEBUG_LOGGING) console.log("content script loaded");
+
+async function handleNavigation(event) {
   const source = location.href;
   if (!source.includes("/status/")) return;
   scrollPositionsMap[source] = window.scrollY;
 
-  const destination = evt.destination.url;
+  const destination = event.destination.url;
   if (!(destination in scrollPositionsMap)) return;
 
   // now we know the user has navigated from one tweet page to another
@@ -14,19 +22,53 @@ window.navigation.addEventListener("navigate", async (evt) => {
   await waitUntilPageChange(origReplyTarget);
   scrollTo({ left: 0, top: scrollPositionsMap[destination] });
 
-  // TODO: highlight tweet user was just seeing
-});
-
-// helpers
-
-async function waitUntilPageChange(origReplyTarget) {
-  do {
-    await delayMs(50);
-  } while (getReplyTargetTweet() === origReplyTarget);
+  // highlight tweet user was just seeing
+  await waitUntilThereAreVisibleTweets();
+  const dataOfTweetToHighlight = getTweetDetails(origReplyTarget);
+  const tweetToHighlight = findTweetInVisibleArea(dataOfTweetToHighlight);
+  if (tweetToHighlight) await highlightTweet(tweetToHighlight);
 }
 
-function delayMs(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+// twitter-specific helpers
+
+async function highlightTweet(tweetNode) {
+  tweetNode.style.transition = "background-color 250ms linear";
+
+  await delayMs(250);
+  tweetNode.style.backgroundColor = "lightblue";
+
+  await delayMs(250);
+  tweetNode.style.backgroundColor = "transparent";
+}
+
+async function waitUntilPageChange(origReplyTarget) {
+  return waitFor(
+    () => getReplyTargetTweet() !== origReplyTarget,
+    "checkReplyTargetHasChanged"
+  );
+}
+
+async function waitUntilThereAreVisibleTweets() {
+  return delayMs(1000);
+  // return waitFor(checkThereAreVisibleTweets, "checkThereAreVisibleTweets");
+}
+
+function checkThereAreVisibleTweets() {
+  const pointToCheck = {
+    x: window.innerWidth / 2,
+    // looking a bit below the top of the screen to clear the "go back"
+    //   element that twitter has fixed to the top
+    y: 60,
+  };
+
+  if (
+    document.elementFromPoint(pointToCheck.x, pointToCheck.y).parentNode
+      .attributes["aria-label"] === "Timeline: Conversation"
+  ) {
+    return false;
+  }
+
+  return true;
 }
 
 function getReplyTargetTweet() {
@@ -38,12 +80,76 @@ function getReplyTargetTweet() {
   return cellInnerDiv.querySelector("article");
 }
 
-function getTweetText(tweetNode) {
-  return tweetNode.querySelector('div[data-testid="tweetText"]').innerText;
+function findTweetInVisibleArea(dataOfTweetToFind) {
+  let tweetToCheck = getFirstVisibleTweet();
+
+  while (tweetToCheck && nodeIsVisible(tweetToCheck)) {
+    const dataOfTweetToCheck = getTweetDetails(tweetToCheck);
+    if (checkTweetsAreSame(dataOfTweetToCheck, dataOfTweetToFind)) {
+      return tweetToCheck;
+    }
+
+    tweetToCheck = getNextTweetOnPage(tweetToCheck);
+  }
+
+  return null;
 }
 
-function getTweetAriaLabel(tweetNode) {
-  return tweetNode.attributes["aria-labelledby"].value;
+// WARN: actually only checks if top of node is visible
+function nodeIsVisible(node) {
+  const rect = node.getBoundingClientRect();
+  return rect.top > 0 && rect.top < window.innerHeight;
+}
+
+function getFirstVisibleTweet() {
+  const pointToCheck = {
+    x: window.innerWidth / 2,
+    // looking a bit below the top of the screen to clear the "go back"
+    //   element that twitter has fixed to the top
+    y: 60,
+  };
+
+  let elemAtTopOfPage = document.elementFromPoint(
+    pointToCheck.x,
+    pointToCheck.y
+  );
+  while (!nodeIsCellInnerDiv(elemAtTopOfPage)) {
+    elemAtTopOfPage = elemAtTopOfPage.parentNode;
+    if (elemAtTopOfPage === null) return null;
+  }
+
+  const foundTweet = elemAtTopOfPage.querySelector("article");
+
+  return nodeIsVisible(foundTweet)
+    ? foundTweet
+    : getNextTweetOnPage(foundTweet);
+}
+
+function getNextTweetOnPage(tweetNode) {
+  let cellInnerDiv = tweetNode;
+  while (!nodeIsCellInnerDiv(cellInnerDiv)) {
+    cellInnerDiv = cellInnerDiv.parentNode;
+  }
+
+  let nextTweet;
+  while (!nextTweet) {
+    cellInnerDiv = cellInnerDiv.nextElementSibling;
+    if (!cellInnerDiv) return null;
+
+    nextTweet = cellInnerDiv.querySelector("article");
+  }
+
+  return nextTweet;
+}
+
+function nodeIsCellInnerDiv(node) {
+  return node.attributes?.["data-testid"]?.value === "cellInnerDiv";
+}
+
+function getTweetText(tweetNode) {
+  return (
+    tweetNode.querySelector('div[data-testid="tweetText"]')?.innerText ?? ""
+  );
 }
 
 function getTweetTime(tweetNode) {
@@ -51,10 +157,6 @@ function getTweetTime(tweetNode) {
 }
 
 function getTweetAuthor(tweetNode) {
-  const avatarUrl = tweetNode
-    .querySelector('div[data-testid="Tweet-User-Avatar"]')
-    .querySelector("img").src;
-
   const userDetailsContainer = tweetNode.querySelector(
     'div[data-testid="User-Name"]'
   );
@@ -64,24 +166,47 @@ function getTweetAuthor(tweetNode) {
       "a"
     ).innerText;
 
-  return {
-    name,
-    username,
-    avatarUrl,
-  };
+  return { name, username };
 }
 
 function getTweetDetails(tweetNode) {
   return {
     author: getTweetAuthor(tweetNode),
-    ariaLabel: getTweetAriaLabel(tweetNode),
     time: getTweetTime(tweetNode),
     text: getTweetText(tweetNode),
   };
 }
 
-function tweetsAreSame(tweetNodeA, tweetNodeB) {
-  const a = getTweetDetails(tweetNodeA);
-  const b = getTweetDetails(tweetNodeB);
-  return a.author.username === b.author.username && a.time === b.time;
+function checkTweetsAreSame(tweetDataA, tweetDataB) {
+  return (
+    tweetDataA.author.username === tweetDataB.author.username &&
+    tweetDataA.time === tweetDataB.time
+  );
+}
+
+// generic helpers
+
+function delayMs(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function waitFor(predFn, name) {
+  const MAX_TRIES = 20;
+  const DELAY_BETWEEN_TRIES_MS = 50;
+
+  const loggingPrefix = `waitFor${name ? `: ${name}` : ""}`;
+  let nTries = 0;
+
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
+    if (predFn()) break;
+
+    nTries++;
+    if (nTries >= MAX_TRIES) {
+      throw new Error(`${loggingPrefix}: max tries exceeded`);
+    }
+
+    if (ENABLE_DEBUG_LOGGING) console.log(`${loggingPrefix}: waiting ...`);
+    await delayMs(DELAY_BETWEEN_TRIES_MS);
+  }
 }
